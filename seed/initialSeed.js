9ec2f825405193f66a2d505abc57cef68ed8e3e9
Alix Fachin <alix.fachin@me.com>
2021-05-04 15:05:36 +0900

    Bug fix (foreign key deleted first) for seeding file

--- seed/initialSeed.js
+++ seed/initialSeed.js
@@ -4,19 +4,19 @@ exports.seed = function(knex) {
   // First, seeding the book entries
   let userIdArray;
   let bookIdArray;
-  return Promise.all([
-    knex('books').del()
-        .then(function() {
-        // Inserts seed entries
-          return knex('books').insert(data.books);
-        }),
-    knex('users').del()
-        .then(() => {
-          return knex('users').insert(data.users);
-        }),
-    // Inventory relationships : User 1 owns Book1 and Book2, User 2 just owns Book 3
-    knex('users_books').del(),
-  ]).then( () => {
+  return knex('users_books').del().then(() => {
+    return Promise.all([
+      knex('books').del()
+          .then(function() {
+            // Inserts seed entries
+            return knex('books').insert(data.books);
+          }),
+      knex('users').del()
+          .then(() => {
+            return knex('users').insert(data.users);
+          })
+    ]);
+  }).then( () => {
     return knex('users').select('*').then((data) => {
       userIdArray = data.map((userObject) => userObject.id);
       return knex('books').select('*');
